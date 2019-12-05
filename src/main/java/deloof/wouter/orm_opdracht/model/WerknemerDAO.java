1523950328728f05a639276479272f6e4bb028c2
Gwouten <wouter.deloof@gmail.com>
2019-12-05 15:15:13 +0100

    Added query to search based on 'naam'

--- src/main/java/deloof/wouter/orm_opdracht/model/WerknemerDAO.java
+++ src/main/java/deloof/wouter/orm_opdracht/model/WerknemerDAO.java
@@ -3,4 +3,8 @@ package deloof.wouter.orm_opdracht.model;
 import org.springframework.data.repository.CrudRepository;
 
 public interface WerknemerDAO extends CrudRepository<Werknemer, Integer> {
+
+    // Zoek op naam
+    Iterable<Werknemer> findByNaam( String naam );
+
 }
