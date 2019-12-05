e8388df31fb9b248545e68cdcf350ac95b2e4bfb
mobapp15 <mobapp15@student.ehb.be>
2019-12-05 14:31:29 +0100

    Initial commit

---
+++ src/main/java/deloof/wouter/orm_opdracht/OrmOpdrachtApplication.java
@@ -0,0 +1,13 @@
+package deloof.wouter.orm_opdracht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmOpdrachtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrmOpdrachtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/deloof/wouter/orm_opdracht/OrmOpdrachtApplicationTests.java
@@ -0,0 +1,13 @@
+package deloof.wouter.orm_opdracht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmOpdrachtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
